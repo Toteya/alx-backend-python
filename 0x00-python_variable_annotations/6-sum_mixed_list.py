4f54e42e206c4b33c12b3d666e6c52498c61c1bf
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-04 12:52:43 +0200

    Sum of a list of ints and floats

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+    """
+    Returns the sum of a list of floats and integers
+    """
+    return sum(mxd_lst)
