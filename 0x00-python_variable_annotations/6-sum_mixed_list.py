776157bb39812e5a99b1a8bdc1f1eed40e4e4ab1
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-04 15:59:47 +0200

    Fixed: types - use typing List, Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-
+module: 6-sum_mixed_list
 """
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of a list of floats and integers
     """
