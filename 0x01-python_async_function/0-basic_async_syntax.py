3acf79873917c5ebfc35fda8b9357daf5fc58a63
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-08 11:15:15 +0200

    Task 0: basic async syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/binenv python3
+""""
+module 0-basic_async_syntax
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    async coroutine that wait for a random delay between 0 and the given
+    max_delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
