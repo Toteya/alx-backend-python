b9125144c505b754031d7062a35d164de4d511d0
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-09 15:46:52 +0200

    Fixed type annotation AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,12 +7,10 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously generates 10 random floats between 0 and 10
     """
-    count = 0
-    while count < 10:
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        count += 1
