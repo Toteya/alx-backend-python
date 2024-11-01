50839d5cf7c347921f797ab8ee266ed096c2b9aa
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-01 16:54:21 +0200

    Task 1: Test for errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ Tests the utils.access_nested_map function """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Tests the utils.access_nested_map function Error conditions """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
