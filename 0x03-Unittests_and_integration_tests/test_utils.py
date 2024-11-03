c27f1cc7e5847a97c14ac818aaf2e86209771f34
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-03 12:59:50 +0200

    unittest mock get request

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,11 +4,12 @@ module: test_utils
 test utils
 """
 from parameterized import parameterized
-import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+from unittest import TestCase
+from utils import access_nested_map, get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """
     Tests utils.access_nested_map
     """
@@ -29,3 +30,20 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """ Tests the utils.access_nested_map function Error conditions """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """
+    Tests utils.get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """Tests utils.get_json that returns a json from a get request"""
+        mock_requests_get.return_value.json.return_value = test_payload
+
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_requests_get.assert_called_once_with(test_url)
