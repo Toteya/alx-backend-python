facb25caab0aa067e5b7df8a415af38912fbd640
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-03 18:23:39 +0200

    called once

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ test utils
 from parameterized import parameterized
 from unittest.mock import patch
 from unittest import TestCase
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
@@ -42,8 +42,40 @@ class TestGetJson(TestCase):
     ])
     @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_requests_get):
-        """Tests utils.get_json that returns a json from a get request"""
+        """Tests utils.get_json that returns a json from a get request
+        """
         mock_requests_get.return_value.json.return_value = test_payload
 
         self.assertEqual(get_json(test_url), test_payload)
         mock_requests_get.assert_called_once_with(test_url)
+    
+
+class TestMemoize(TestCase):
+    """
+    Tests utils.memoize
+    """
+    def test_memoize(self):
+        """ Tests the utils.memoize decorator that memoizes a method
+        """
+        class TestClass:
+            
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+            test_obj = TestClass()
+            response1 = test_obj.a_property
+            response2 = test_obj.a_property
+        
+        self.assertEqual(response1, 42)
+        self.assertEqual(response2, 42)
+        mock_a_method.assert_called_once()
+
+        
+
+
