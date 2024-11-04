d5a62e19168f2b9f50604dd17b73898bef6e1925
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-04 19:52:13 +0200

    Revert "integrated tests"
    This reverts commit 213a839b03fa30b7483fe02ed3bb13dc99d0e89c.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,7 @@
 test_client: contains unittests for client module
 """
 from client import GithubOrgClient
-import fixtures
 from parameterized import parameterized
-from parameterized import parameterized_class
 from unittest import TestCase
 from unittest.mock import patch
 from unittest.mock import PropertyMock
@@ -24,7 +22,7 @@ class TestGithubOrgClient(TestCase):
     def test_org(self, org_name, mock_get_json):
         """ Tests org method
         """
-        fake_json = {'Key': 'DATA'}
+        fake_json = {'Status': 'OK'}
         mock_get_json.return_value = fake_json
         obj = GithubOrgClient(org_name)
         url = 'https://api.github.com/orgs/{}'.format(org_name)
@@ -75,12 +73,12 @@ class TestGithubOrgClient(TestCase):
             new_callable=PropertyMock
         ) as mock_public_repos_url:
             obj = GithubOrgClient('google')
-            result_expected_repos = [
+            fake_payload_repos = [
                 "ruby-openid-apps-discovery",
                 "anvil-build",
                 "googletv-android-samples",
             ]
-            self.assertEqual(obj.public_repos(), result_expected_repos)
+            self.assertEqual(obj.public_repos(), fake_payload_repos)
             mock_get_json.assert_called_once()
             mock_public_repos_url.assert_called_once()
 
@@ -92,42 +90,3 @@ class TestGithubOrgClient(TestCase):
         """ Tests GithubOrgClient.has_license method
         """
         self.assertEqual(GithubOrgClient.has_license(repo, key), expected)
-
-
-@parameterized_class([
-    {'repos_payload': fixtures.TEST_PAYLOAD[0][0]},
-    {'expected_repos': fixtures.TEST_PAYLOAD[0][1]},
-    {'org_payload': fixtures.TEST_PAYLOAD[0][1][1]['owner']},
-    {'apache2_repos': fixtures.TEST_PAYLOAD[0][3]}
-])
-class TestIntegrationGithubOrgClient(TestCase):
-    """
-    Integration test for client.GithubOrgClient
-    """
-    @classmethod
-    def setUpClass(cls, get_patcher):
-        """ Initialises testing paramters for the test class
-        """
-        # fake_json = fixtures.TEST_PAYLOAD.
-        cls.get_patcher = patch('utils.requests.get')
-        mock_request_get = get_patcher.start()
-
-        # mock_request_get.return_value.json.return_value
-
-        def request_get(url):
-            """ Side effect of the mock get request to allow the given url to
-            be matched to the correct payload
-            """
-            if url == 'https://api.github.com/orgs/google':
-                return cls.org_payload
-            if url == "https://api.github.com/orgs/google/repos":
-                return cls.expected_repos
-
-        get_patcher.side_effect = request_get
-
-    @classmethod
-    def tearDownClass(cls):
-        """ Tears down test conditions for the class
-        """
-        cls.get_patcher.stop()
-        pass
